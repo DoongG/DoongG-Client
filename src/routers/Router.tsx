9d8042f1d77827467c53e438560a2fbf76ea2e9d
7jjin <wlsgud6305@naver.com>
2023-11-13 20:09:31 +0900

    Router

--- src/routers/Router.tsx
+++ src/routers/Router.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Main } from '../pages/Main';
 import { Board } from '../pages/Board';
+import { Shopping } from '../pages/Shopping';
 
 const Router = () => {
     return (
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Board />}></Route>
+                    <Route path="/" element={<Main />}></Route>
+                    <Route path="/board" element={<Board />}></Route>
+                    <Route path="/shopping" element={<Shopping />}></Route>
                 </Routes>
             </BrowserRouter>
         </>
