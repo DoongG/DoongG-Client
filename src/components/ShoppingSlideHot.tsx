6a3da06a3c2d08f1ef485ab3552652afc2e2502c
7jjin <wlsgud6305@naver.com>
2023-11-24 20:28:26 +0900

    feat axios 데이터 받아오기

--- src/components/ShoppingSlideHot.tsx
+++ src/components/ShoppingSlideHot.tsx
@@ -1,184 +1,216 @@
-import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-regular-svg-icons";
-import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
-import "swiper/swiper-bundle.css";
-import styled from "styled-components";
-import shopping1 from "../assets/shopping1.jpg";
-import shopping2 from "../assets/shopping2.jpg";
-import shopping3 from "../assets/shopping3.jpg";
-import shopping4 from "../assets/shopping4.jpg";
-import shopping5 from "../assets/shopping5.jpg";
+import React, { useEffect, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import axios from 'axios';
+import eyes from '../assets/eyes.png';
+import SwiperCore, {
+    Navigation,
+    Pagination,
+    Scrollbar,
+    A11y,
+} from 'swiper/modules';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import 'swiper/swiper-bundle.css';
+import styled from 'styled-components';
+import shopping1 from '../assets/shopping1.jpg';
+import shopping2 from '../assets/shopping2.jpg';
+import shopping3 from '../assets/shopping3.jpg';
+import shopping4 from '../assets/shopping4.jpg';
+import shopping5 from '../assets/shopping5.jpg';
+
+interface ApiResponse {
+    discountedPrice: number;
+    price: number;
+    productID: number;
+    productImage: string;
+    productName: string;
+    stock: number;
+    viewCount: number;
+}
+
+export default function ShoppingSlideResent() {
+    const [resentProductList, setResentProductList] = useState<ApiResponse[]>(
+        [],
+    );
+
+    // 천 단위 쉼표 추가 함수
+    const addCommas = (num: number) => {
+        return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    };
+
+    // 할인률 구하는 함수
+    function calculateDiscountRate(
+        originalPrice: number,
+        discountedPrice: number,
+    ) {
+        // 원래 가격과 할인된 가격을 이용하여 할인률을 계산합니다.
+        const discountAmount = originalPrice - discountedPrice;
+        const discountRate = (discountAmount / originalPrice) * 100;
+
+        return discountRate;
+    }
+
+    useEffect(() => {
+        const getResentProduct = async () => {
+            try {
+                const res = await axios.get<ApiResponse, any>(
+                    'http://localhost:8080/shop/best',
+                );
+                console.log(res.data);
+                setResentProductList(res.data);
+            } catch (error) {
+                console.error('데이터를 가져오는 중 오류 발생:', error);
+            }
+        };
+        getResentProduct();
+    }, []);
+
+    return (
+        <>
+            <_swiperWrapper className="swiperWrppaer">
+                <_customSwiper
+                    modules={[Navigation, Pagination]}
+                    spaceBetween={50} // 이미지 간격
+                    slidesPerView={5} // 한번에 보이는 이미지 수
+                    slidesPerGroup={1} // 그룹 당 슬라이드 수 설정
+                    navigation
+                >
+                    {resentProductList.map((item: ApiResponse) => {
+                        return (
+                            <>
+                                <_customSwiperSlide className="swiperslide">
+                                    <_contentWrapper className="contentWrapper">
+                                        <_favoriteDiv className="favoriteDiv">
+                                            <img src={eyes} alt="" />
+                                            <p>{item.viewCount}</p>
+                                        </_favoriteDiv>
+                                        <img src={shopping1} alt="이미지1" />
+                                        <span>{item.productName}</span>
+                                        <_productInfos>
+                                            <_perDiv className="perDiv">
+                                                {calculateDiscountRate(
+                                                    item.price,
+                                                    item.discountedPrice,
+                                                ).toFixed(0)}
+                                                %
+                                            </_perDiv>
+                                            <_priceDiv className="priceDiv">
+                                                <_initPriceDiv className="initPriceDiv">
+                                                    {addCommas(item.price)}원
+                                                </_initPriceDiv>
+                                                <_realPriceDiv className="realPriceDiv">
+                                                    {addCommas(
+                                                        item.discountedPrice,
+                                                    )}
+                                                    원
+                                                </_realPriceDiv>
+                                            </_priceDiv>
+                                        </_productInfos>
+                                    </_contentWrapper>
+                                </_customSwiperSlide>
+                            </>
+                        );
+                    })}
+                </_customSwiper>
+            </_swiperWrapper>
+        </>
+    );
+}
 
 // Swiper Custom
 const _customSwiper = styled(Swiper)`
-  width: 90%;
-  height: 350px;
-  margin: 0 !important;
-  padding: 0px 50px;
-  // 슬라이드 버튼
-  .swiper-button-prev,
-  .swiper-button-next {
-    position: absolute;
-    top: 110px;
-  }
-  .swiper-button-next {
-    right: 0px;
-  }
-  .swiper-button-prev {
-    left: 0px;
-  }
-  img {
-    border-radius: 15px;
-    width: 100%;
-    height: 95%;
-  }
-  span {
-    display: -webkit-box;
-    text-align: left;
-    -webkit-line-clamp: 2; /* 표시할 줄 수 */
-    -webkit-box-orient: vertical;
-    overflow: hidden;
-    margin-top: 10px;
-    white-space: pre-line; /* 공백 유지 */
-    min-height: 42px;
-  }
+    width: 90%;
+    height: 350px;
+    margin: 0 !important;
+    padding: 0px 50px;
+    // 슬라이드 버튼
+    .swiper-button-prev,
+    .swiper-button-next {
+        position: absolute;
+        top: 110px;
+    }
+    .swiper-button-next {
+        right: 0px;
+    }
+    .swiper-button-prev {
+        left: 0px;
+    }
+    img {
+        border-radius: 15px;
+        width: 100%;
+        height: 95%;
+    }
+    span {
+        display: -webkit-box;
+        text-align: left;
+        -webkit-line-clamp: 2; /* 표시할 줄 수 */
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+        margin-top: 10px;
+        white-space: pre-line; /* 공백 유지 */
+        min-height: 42px;
+    }
 `;
 // SwiperSlide Custom
 const _customSwiperSlide = styled(SwiperSlide)`
-  height: 220px;
+    height: 220px;
+`;
+
+const _favoriteDiv = styled.div`
+    display: flex;
+    position: absolute;
+    top: 15px;
+    left: 15px;
+    img {
+        margin-right: 5px;
+        width: 20px;
+    }
+    p {
+        margin: 0;
+    }
+`;
+
+// 아이콘 커스텀
+const _customFontAwesome = styled(FontAwesomeIcon)`
+    font-size: 25px;
+    margin-right: 7px;
 `;
 
 const _swiperWrapper = styled.div`
-  background-color: white;
-  padding: 20px 80px 32px;
-  position: relative;
-  .swiper-wrapper {
-    transition-timing-function: linear !important;
-    transition-duration: 500ms !important;
-  }
+    padding: 20px 80px 32px;
+    position: relative;
+    background-color: white;
+
+    .swiper-wrapper {
+        transition-timing-function: linear !important;
+        transition-duration: 500ms !important;
+    }
 `;
 
 const _contentWrapper = styled.div`
-  width: 100%;
-  height: 100%;
+    width: 100%;
+    height: 100%;
 `;
 
 const _productInfos = styled.div`
-  display: flex;
-  margin-top: 10px;
+    display: flex;
+    margin-top: 10px;
 `;
 
 const _perDiv = styled.div`
-  font-weight: 700;
-  font-size: 20px;
-  color: red;
+    font-weight: 700;
+    font-size: 20px;
+    color: red;
 `;
 const _priceDiv = styled.div`
-  margin-left: 15px;
-  margin-top: 5px;
+    margin-left: 15px;
+    margin-top: 5px;
 `;
 
 const _initPriceDiv = styled.div`
-  text-decoration: line-through;
+    text-decoration: line-through;
 `;
 const _realPriceDiv = styled.div`
-  font-weight: 700;
-  margin-top: 5px;
+    font-weight: 700;
+    margin-top: 5px;
 `;
-
-export default function ShoppingSlideHot() {
-  return (
-    <>
-      <_swiperWrapper className="swiperWrppaer">
-        <_customSwiper
-          modules={[Navigation, Pagination]}
-          spaceBetween={50} // 이미지 간격
-          slidesPerView={5} // 한번에 보이는 이미지 수
-          slidesPerGroup={1} // 그룹 당 슬라이드 수 설정
-          navigation
-        >
-          <_customSwiperSlide className="swiperslide">
-            <_contentWrapper className="contentWrapper">
-              <img src={shopping4} alt="이미지1" />
-              <span>부침개</span>
-              <_productInfos>
-                <_perDiv className="perDiv">14%</_perDiv>
-                <_priceDiv className="priceDiv">
-                  <_initPriceDiv className="initPriceDiv">10,000원</_initPriceDiv>
-                  <_realPriceDiv className="realPriceDiv">8,600원</_realPriceDiv>
-                </_priceDiv>
-              </_productInfos>
-            </_contentWrapper>
-          </_customSwiperSlide>
-          <_customSwiperSlide>
-            <_contentWrapper className="contentWrapper">
-              <img src={shopping3} alt="이미지1" />
-              <span>부침개 논란 종결 해물파전/김치전 반반</span>
-              <_productInfos>
-                <_perDiv className="perDiv">14%</_perDiv>
-                <_priceDiv className="priceDiv">
-                  <_initPriceDiv className="initPriceDiv">10,000원</_initPriceDiv>
-                  <_realPriceDiv className="realPriceDiv">8,600원</_realPriceDiv>
-                </_priceDiv>
-              </_productInfos>
-            </_contentWrapper>
-          </_customSwiperSlide>
-          <_customSwiperSlide>
-            <_contentWrapper className="contentWrapper">
-              <img src={shopping2} alt="이미지1" />
-              <span>부침개 논란 종결 해물파전</span>
-              <_productInfos>
-                <_perDiv className="perDiv">14%</_perDiv>
-                <_priceDiv className="priceDiv">
-                  <_initPriceDiv className="initPriceDiv">10,000원</_initPriceDiv>
-                  <_realPriceDiv className="realPriceDiv">8,600원</_realPriceDiv>
-                </_priceDiv>
-              </_productInfos>
-            </_contentWrapper>
-          </_customSwiperSlide>
-          <_customSwiperSlide>
-            <_contentWrapper className="contentWrapper">
-              <img src={shopping1} alt="이미지1" />
-              <span>부침개 논란 종결 해물파전/김치전 반반</span>
-              <_productInfos>
-                <_perDiv className="perDiv">14%</_perDiv>
-                <_priceDiv className="priceDiv">
-                  <_initPriceDiv className="initPriceDiv">10,000원</_initPriceDiv>
-                  <_realPriceDiv className="realPriceDiv">8,600원</_realPriceDiv>
-                </_priceDiv>
-              </_productInfos>
-            </_contentWrapper>
-          </_customSwiperSlide>
-          <_customSwiperSlide>
-            <_contentWrapper className="contentWrapper">
-              <img src={shopping5} alt="이미지1" />
-              <span>부침개 논란 종결</span>
-              <_productInfos>
-                <_perDiv className="perDiv">14%</_perDiv>
-                <_priceDiv className="priceDiv">
-                  <_initPriceDiv className="initPriceDiv">10,000원</_initPriceDiv>
-                  <_realPriceDiv className="realPriceDiv">8,600원</_realPriceDiv>
-                </_priceDiv>
-              </_productInfos>
-            </_contentWrapper>
-          </_customSwiperSlide>
-          <_customSwiperSlide>
-            <_contentWrapper className="contentWrapper">
-              <img src={shopping1} alt="이미지1" />
-              <span>부침개 논란 종결 해물파전/김치전 반반</span>
-              <_productInfos>
-                <_perDiv className="perDiv">14%</_perDiv>
-                <_priceDiv className="priceDiv">
-                  <_initPriceDiv className="initPriceDiv">10,000원</_initPriceDiv>
-                  <_realPriceDiv className="realPriceDiv">8,600원</_realPriceDiv>
-                </_priceDiv>
-              </_productInfos>
-            </_contentWrapper>
-          </_customSwiperSlide>
-        </_customSwiper>
-      </_swiperWrapper>
-    </>
-  );
-}
